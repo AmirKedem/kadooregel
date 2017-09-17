72630867c7a55e0da9f8322fdfc1f28e5869a891
1ZouLTReX1 <amir.trex@gmail.com>
2017-09-17 15:47:24 +0300

    Delete ball.js

--- public/ball.js
+++
@@ -1,6 +0,0 @@
-function renderBall() {
-	var ballColBlue = map(ballpos.x,courtwidth/2,3*courtwidth/4,0,255);
-	var ballColRed = map(ballpos.x,courtwidth/4,courtwidth/2,255,0);
-  fill(ballColRed,0,ballColBlue);
-	ellipse(ballpos.x,ballpos.y,ballsize*2);
-}
